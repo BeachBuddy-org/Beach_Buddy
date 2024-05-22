e8e31671b857bbe3b36a588cf976532c6fb630da
TomasPegado <lenzitomas00@gmail.com>
2024-05-22 14:58:54 -0300

    Componente Model

---
+++ api/model/__init__.py
@@ -0,0 +1,34 @@
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import os
+
+
+from model.Aluno import Aluno
+from model.Usuario import Usuario
+
+
+# Definindo path para o banco de dados
+db_path = "database/"
+
+
+# Verificando a exitência do diretório
+if not os.path.exists(db_path):
+    os.makedirs(db_path)
+    
+# Definindo URL para o banco de dados
+db_url = f'sqlite:///{db_path}/db.sqlite3' 
+
+# Criando engine para realizar a conexão com o bd
+# echo=False para não exibir as queries executadas
+engine = create_engine(db_url, echo=False)
+
+# Instancia um criador de seção com o banco
+Session = sessionmaker(bind=engine)
+
+# Criando o banco de dados se ele não existir
+if not database_exists(engine.url):
+    create_database(engine.url)
+    
+# Criando as tabelas do banco de dados, caso não existam
+Base.metadata.create_all(engine)
