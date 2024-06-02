395d1a97849fa2c84ba8471687a5aaaab4313a58
em1ru <gabemile7@gmail.com>
2024-06-02 00:07:57 -0300

    Adiciona relação presencas no modelo Aluno

--- backend/app/models/aluno.py
+++ backend/app/models/aluno.py
@@ -1,13 +1,14 @@
-
 from app import db
 from .usuario import Usuario
 from .aluno_ct_association import aluno_ct_association
+from .presenca import Presenca
 
 class Aluno(Usuario):
     __tablename__ = 'aluno'
     id = db.Column(db.Integer, db.ForeignKey('usuario.id'), primary_key=True)
 
     cts = db.relationship('CT', secondary=aluno_ct_association, back_populates='alunos')
+    presencas = db.relationship('Presenca', back_populates='aluno', cascade='all, delete-orphan') 
 
     __mapper_args__ = {
         'polymorphic_identity': 'aluno',
@@ -19,9 +20,3 @@ class Aluno(Usuario):
     def __str__(self):
         return (f'Aluno(id={self.id}, username={self.username}, email={self.email}, '
                 f'first_name={self.first_name}, last_name={self.last_name}')
-
-
-
-    
-    
-    
